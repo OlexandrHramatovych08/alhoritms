7f8bae49dfae6c838e8b5b81853ed0834a0c86a3
Olexandr_Hramatovych <hramatovych.oleksandr@chnu.edu.ua>
2024-04-20 14:04:37 +0300

    done binary search algorithm

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,22 @@
+def binery_search(min,max,goal,count = 1):
+    middle = (min+max)//2
+    print("{}) {}".format(count,middle))
+    if middle == goal:
+        print (f"Done with {count} counts")
+        return count
+    while middle != goal:
+        if middle > goal:
+            count += 1
+            binery_search(min,middle,goal,count)
+        if middle < goal:
+            count += 1
+            binery_search(middle,max,goal,count)
+        break
+
+num1 = int(input("Enter the first number(minimum of diapason): "))
+
+num2 = int(input("Enter the second number(maximum of diapason): "))
+goal = int(input("Enter the goal that you want o achieve: "))
+print(binery_search(num1,num2,goal))
+
 
